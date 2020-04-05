27104f5af3a45ed1b8314640ed831973735f3781
osoukup <osoukup.mail@gmail.com>
2020-04-05 18:03:11 +0200

    shelcheck-suggested improvements

--- vim/setup-vim.sh
+++ vim/setup-vim.sh
@@ -7,12 +7,12 @@ sudo yum install git -y
 sudo yum install vim -y
 
 rm -f ~/.vimrc
-ln -s $DIR/vimrc ~/.vimrc
+ln -s "$DIR"/vimrc ~/.vimrc
 
 rm -rf ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-for repo in $( cat $DIR/setup-vim.conf ); do
-    git clone --recursive $repo ~/.vim/bundle/$( basename $repo | cut -d. -f1 )
-done
+while read -r repo; do
+    git clone --recursive "$repo" ~/.vim/bundle/"$( basename "$repo" | cut -d. -f1 )"
+done < "$DIR"/setup-vim.conf
